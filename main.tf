e9817e5b0cf67e9864f037a13083b870793593dd
Pawel Switala <p.switala@pscloud.pl>
2020-01-01 20:20:11 +0100

    Updated vars names

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "pslcoud_vpc" {
+resource "aws_vpc" "pslcoud-vpc" {
   cidr_block           = "${var.pscloud_cidr_block}.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -9,7 +9,7 @@ resource "aws_vpc" "pslcoud_vpc" {
 }
 
 resource "aws_internet_gateway" "pscloud-gw" {
-  vpc_id = aws_vpc.pslcoud_vpc.id
+  vpc_id = aws_vpc.pslcoud-vpc.id
 
   tags = {
     Name = "${var.pscloud_company}_gw_${var.pscloud_env}"
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "pscloud-gw" {
 }
 
 resource "aws_route_table" "pscloud-rt-public" {
-  vpc_id = aws_vpc.pslcoud_vpc.id
+  vpc_id = aws_vpc.pslcoud-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -30,7 +30,7 @@ resource "aws_route_table" "pscloud-rt-public" {
 }
 
 resource "aws_route_table" "pscloud-rt-private" {
-  vpc_id = aws_vpc.pslcoud_vpc.id
+  vpc_id = aws_vpc.pslcoud-vpc.id
 
   tags = {
     Name = "${var.pscloud_company}_rt_private_${var.pscloud_env}"
@@ -39,7 +39,7 @@ resource "aws_route_table" "pscloud-rt-private" {
 
 resource "aws_subnet" "pscloud-private" {
   count             = length(var.pscloud_az)
-  vpc_id            = aws_vpc.pslcoud_vpc.id
+  vpc_id            = aws_vpc.pslcoud-vpc.id
   availability_zone = element(var.pscloud_az, count.index)
   cidr_block        = "${var.pscloud_cidr_block}.1${count.index}.0/24"
 
@@ -50,7 +50,7 @@ resource "aws_subnet" "pscloud-private" {
 
 resource "aws_subnet" "pscloud-public" {
   count                   = length(var.pscloud_az)
-  vpc_id                  = aws_vpc.pslcoud_vpc.id
+  vpc_id                  = aws_vpc.pslcoud-vpc.id
   availability_zone       = element(var.pscloud_az, count.index)
   cidr_block              = "${var.pscloud_cidr_block}.${count.index}.0/24"
   map_public_ip_on_launch = "true"
@@ -60,7 +60,7 @@ resource "aws_subnet" "pscloud-public" {
   }
 }
 
-resource "aws_db_subnet_group" "tf_rds_subnet_group" {
+resource "aws_db_subnet_group" "pslouc-rds-subnet-group" {
   name = "${var.pscloud_company}_rds_subnet_group_${var.pscloud_env}"
   subnet_ids = [
     for as in aws_subnet.pscloud-private :
@@ -72,13 +72,13 @@ resource "aws_db_subnet_group" "tf_rds_subnet_group" {
   }
 }
 
-resource "aws_route_table_association" "assoc_public" {
+resource "aws_route_table_association" "assoc-public" {
   count          = length(aws_subnet.pscloud-public)
   subnet_id      = element(aws_subnet.pscloud-public, count.index).id
   route_table_id = aws_route_table.pscloud-rt-public.id
 }
 
-resource "aws_route_table_association" "assoc_private" {
+resource "aws_route_table_association" "assoc-private" {
   count          = length(aws_subnet.pscloud-private)
   subnet_id      = element(aws_subnet.pscloud-private, count.index).id
   route_table_id = aws_route_table.pscloud-rt-private.id
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "pscloud_vpc" {
+  value = aws_vpc.pslcoud-vpc
+}
+
 output "pscloud_public_subnets_ids" {
   value = aws_subnet.pscloud-public
 }
@@ -6,9 +10,5 @@ output "pscloud_private_subnets_ids" {
   value = aws_subnet.pscloud-private
 }
 output "pscloud_rds_subnet_group" {
-  value = aws_db_subnet_group.tf_rds_subnet_group.id
-}
-
-output "pscloud_vpc_id" {
-  value = aws_vpc.pslcoud_vpc.id
+  value = aws_db_subnet_group.pslouc-rds-subnet-group
 }
